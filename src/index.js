4f5092ed8948b93155c7841ddfc262bb6147a032
annapik <anna.pikulskaya@yandex.ru>
2022-01-15 23:42:45 +0400

    feat: complete function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
-}
+    let i = 0;
+    let result = '';
+    let strNum = String(Math.abs(n));
+    while (i < strNum.length) {
+      result = `${strNum[i]}${result}`;
+      i = i + 1;
+    }
+    return Number(result);
+  };
+
